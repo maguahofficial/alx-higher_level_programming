6e3ef39f4a66b9b0b0291e5e8f470e41289da7bc
maguah <maguahofficial@gmail.com>
2023-12-01 04:53:20 +0300

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
-    from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
+    nargsvarble = len(sys.argv) - 1
+    if nargsvarble != 3:
         sys.exit(1)
 
-    opsvv = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(opsvv.keys()):
+    opvarble = sys.argv[2]
+    if opvarble != '+' and opvarble != '-' and opvarble != '*' and opvarble != '/':
         sys.exit(1)
 
+    from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, opsvv[sys.argv[2]](a, b)))
+
+    if opvarble == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif opvarble == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif opvarble == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
