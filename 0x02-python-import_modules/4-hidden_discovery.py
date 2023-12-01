a2d8626825d640225c04265b636992796dc745c8
maguah <maguahofficial@gmail.com>
2023-12-01 03:27:27 +0300

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-if __namevarble__ == "__main__":
-    """Print all names defined by hidden_4 module."""
+if __name__ == "__main__":
+    """Code Prints all names defined by hidden_4 module."""
     import hidden_4
 
-    namesvarble = dir(hidden_4)
-    for namevarble in names:
-        if namevarble[:2] != "__":
-            print(namevarble)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
