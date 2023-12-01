df88ed19bd6198324c9f33bb1673d0d1eb11887a
maguah <maguahofficial@gmail.com>
2023-12-01 04:45:57 +0300

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Code Handles basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        sys.exit(1)
+
+    aa = int(sys.argv[1])
+    bb = int(sys.argv[3])
+    print("{} {} {} = {}".format(aa, sys.argv[2], bb, ops[sys.argv[2]](aa, bb)))
+
---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        cvrb = add(a, b)
+        for i in range(4, 6):
+            cvrb = add(cvrb, i)
+        return (cvrb)
+
+    else:
+        return(sub(a, b))
