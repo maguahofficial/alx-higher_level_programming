0cc31d54ed4af6a0ec19f50a51647b57991091e7
maguah <maguahofficial@gmail.com>
2023-12-03 22:15:26 +0300

    python-data_structures

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -3,17 +3,17 @@
 #define LISTS_H
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * struct listint_s - function singly linked list
+ * @n: (variable)integer
+ * @next: (pointer)points to the next node
+ * header file
+ * Description: the singly linked list node structure
  * for project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -22,4 +22,4 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
+#endif
