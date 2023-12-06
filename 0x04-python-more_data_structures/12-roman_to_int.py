c2f0141376f76e86e78345de3dbff1b539d40da2
maguah <maguahofficial@gmail.com>
2023-12-06 22:52:01 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,41 +1,15 @@
 #!/usr/bin/python3
-
-def to_subtract(list_num):
-    to_subvrble = 0
-    max_listvrble = max(list_num)
-
-    for n in list_num:
-        if max_listvrble > n:
-            to_subvrble += n
-
-    return (max_listvrble - to_subvrble)
-
-
-def roman_to_int(roman_string):
-    if not roman_string:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
+    datavrbl = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbersvrbl = [datavrbl[x] for x in roman_string] + [0]
+    repvrbl = 0
 
-    if not isinstance(roman_string, str):
-        return 0
+    for i in range(len(numbersvrbl) - 1):
+        if numbersvrbl[i] >= numbersvrbl[i+1]:
+            repvrbl += numbersvrbl[i]
+        else:
+            repvrbl -= numbersvrbl[i]
 
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keysvrble = list(rom_n.keys())
-
-    numvrble = 0
-    last_romvrble = 0
-    list_num = [0]
-
-    for ch in roman_string:
-        for r_num in list_keysvrble:
-            if r_num == ch:
-                if rom_n.get(ch) <= last_romvrble:
-                    numvrble += to_subtract(list_numvrble)
-                    list_numvrble = [rom_n.get(ch)]
-                else:
-                    list_numvrble.append(rom_n.get(ch))
-
-                last_romvrble = rom_n.get(ch)
-
-    numvrble += to_subtract(list_num)
-
-    return (numvrble)
+    return repvrbl
