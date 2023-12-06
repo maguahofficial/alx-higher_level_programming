76f7f5ca994c863549815a9d8df73ee6642e9516
maguah <maguahofficial@gmail.com>
2023-12-06 23:02:40 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
+    return max(a_dictionary, keys=a_dictionary.get) if a_dictionary else None
 
-    return (max(a_dictionary, key=a_dictionary.get)i)
