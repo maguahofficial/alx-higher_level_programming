7f71197558845a1980ce2cef45e5a44675b40649
maguah <maguahofficial@gmail.com>
2023-12-06 23:30:38 +0300

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    to_subvrb = 0
-    max_listvrb = max(list_num)
 
-    for nx in list_num:
+def to_subtract(list_numvrb):
+    to_subvrb = 0
+    max_listvrb = max(list_numvrb)
+
+    for nx in list_numvrb:
         if max_listvrb > nx:
             to_subvrb += nx
 
@@ -17,24 +18,24 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    rom_vrb = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keysvrb = list(rom_vrb.keys())
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keysvrb = list(rom_n.keys())
 
     numvrb = 0
     last_romvrb = 0
     list_numvrb = [0]
 
-    for chx in roman_string:
-        for r_num in list_keysvrb:
-            if r_num == chx:
-                if rom_vrb.get(chx) <= last_romvrb:
+    for chvrb in roman_string:
+        for r_numvrb in list_keysvrb:
+            if r_numvrb == chvrb:
+                if rom_n.get(chvrb) <= last_romvrb:
                     numvrb += to_subtract(list_numvrb)
-                    list_numvrb = [rom_vrb.get(chx)]
+                    list_numvrb = [rom_n.get(chvrb)]
                 else:
-                    list_numvrb.append(rom_vrb.get(chx))
+                    list_numvrb.append(rom_n.get(chvrb))
 
-                last_romvrb = rom_vrb.get(chx)
+                last_romvrb = rom_n.get(chvrb)
 
-    num += to_subtract(list_numvrb)
+    numvrb += to_subtract(list_numvrb)
 
     return (numvrb)
