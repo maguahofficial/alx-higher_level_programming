d80a1b03f501670d51d3ce4f532d4aff8b811a61
maguah <maguahofficial@gmail.com>
2023-12-19 02:49:37 +0300

    0x05. Python - Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     n_listvarble = []
     for ix in range(list_length):
         try:
-            divvarble = my_list_1[i] / my_list_2[ix]
+            divvarble = my_list_1[ix] / my_list_2[ix]
         except TypeError:
             print("wrong type")
             divvarble = 0
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             divvarble = 0
         finally:
-            n_listvarble.append(div)
+            n_listvarble.append(divvarble)
     return n_listvarble
