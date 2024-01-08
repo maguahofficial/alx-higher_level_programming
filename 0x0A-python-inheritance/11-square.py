d191023fcb2dc18c704130de711e40e7d0627715
maguah <maguahofficial@gmail.com>
2024-01-09 01:54:52 +0300

    Python - Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,10 +4,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """this function represents a square."""
+    """function represents a square."""
 
     def __init__(self, size):
-        """this function initializes a new square.
+        """function initializes a new square.
 
         Args:
             size (int variable):size of the new square.
